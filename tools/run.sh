30c9ffc3936f872b55922a227bca5d717b234d6e
Haruki Sato <haruki-sugarsun@users.noreply.github.com>
2021-08-29 00:37:25 +0900

    Add run.sh to trigger.

---
+++ tools/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./tools/confluence_site_converter --user text@sickhack.net \
+                                  --password ${CONFLUENCE_TOKEN} \
+                                  --root-page-id 506363930 \
+                                  --cache false \
+                                  --cache-dir cache \
+                                  --output-dir _confluence_pages \
+                                  --sleep 1
