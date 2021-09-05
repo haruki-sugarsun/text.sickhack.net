ebf882a48bf50d826e8da2ada89abb1e881480e9
Haruki Sato <haruki-sugarsun@users.noreply.github.com>
2021-09-05 10:47:25 +0900

    Add env-variables to control run.sh behavior.

--- tools/run.sh
+++ tools/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -eu
+
 ./tools/confluence_site_converter --user text@sickhack.net \
                                   --password ${CONFLUENCE_TOKEN} \
                                   --root-page-id 506363930 \
-                                  --cache false \
+                                  --cache ${USE_CACHE:-false} \
                                   --cache-dir cache \
                                   --output-dir _confluence_pages \
-                                  --sleep 1 \
+                                  --sleep ${SLEEP_SEC:-1} \
     > processing.log 2>&1
