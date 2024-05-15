8671c6772808747406a3d1e439c5901c86f02f5d
Haruki Sato <haruki-sugarsun@users.noreply.github.com>
2024-05-15 15:50:49 +0900

    TEMP

--- tools/run.sh
+++ tools/run.sh
@@ -6,7 +6,9 @@ chmod +x ./tools/confluence_site_converter
                                   --user text@sickhack.net \
                                   --password ${CONFLUENCE_TOKEN} \
                                   --root-page-id 506363930 \
+                                  --space-id 507248673 \
                                   --process-one-target-id ${PROCESS_ONE_TARGET:--} \
+                                  --incremental ${INCREMENTAL:-false} \
                                   --cache ${USE_CACHE:-false} \
                                   --cache-dir cache \
                                   --output-dir _confluence_pages \
